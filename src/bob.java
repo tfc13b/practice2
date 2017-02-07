9dc6f9a4637b1ebea4d6e21cf387768d63f33efe
tfc13b <tfc13b@gmail.com>
2017-02-07 13:30:49 -0500

    adding a new program

---
+++ src/bob.java
@@ -0,0 +1,20 @@
+
+public class bob {
+	public static void main(String[] args){
+		
+		int bottle = 99;
+		for (int x = 99; x >1; x--)
+		{
+			System.out.println(bottle + " bottles of beer on the wall" );
+			System.out.println(bottle + " bottles of beer");
+			System.out.println("Take one down, pass it around" );
+			bottle--; 
+			//System.out.println("Take one down, pass it around" );
+			System.out.println(bottle + " bottles of beer on the wall\n" );
+			
+		}
+		for (int x = 0; x >=0; x--)
+		{System.out.println("Theres no more beer better get more!" );
+	}
+	}
+}
